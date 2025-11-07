70bf8a2b0494cb5785bcb350a4b92a59069cb143
MA <hebertmarcantoine@gmail.com>
2025-11-07 11:48:38 -0500

    Delete micro-projects/purview directory

--- micro-projects/purview/powershell/DLP/internal/Get-RuleMinimumCount.ps1
+++
@@ -1,9 +0,0 @@
-function Get-RuleMinimumCount {
-    [CmdletBinding()]
-    param()
-    
-    return Read-Host "Nombre minimum d'occurrences requises (ex: 1)"
-}
-
-
-### REFACTOR
--- micro-projects/purview/powershell/moduleDLP.ps1
+++
@@ -1,5 +0,0 @@
-$moduleDirectory = Join-Path $PSScriptRoot "Dlp"
-Import-Module $moduleDirectory
-
-# Puis utilisez les fonctions du module
-New-CompleteDlpPolicy
